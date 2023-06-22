f7f02498ea83bbdbc1a7185b6269bad9912af7d8
RobsonSousaS <robson.silva09@aluno.ifce.edu.br>
2023-06-22 14:46:56 -0300

    nova att

--- lib/pages/slider.dart
+++ lib/pages/slider.dart
@@ -16,82 +16,84 @@ class _SliderPageState extends State<SliderPage> {
 
   int currentIndex = 0;
   final CarouselController _carouselController = CarouselController();
-  final PageController _pageController = PageController(); // Adicionado PageController
+  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 254, 254, 254),
-      body: Stack(
-        children: [
-          CarouselSlider(
-            carouselController: _carouselController,
-            options: CarouselOptions(
-              height: double.infinity,
-              viewportFraction: 1.0,
-              enableInfiniteScroll: true,
-              autoPlay: false,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: true,
-              scrollDirection: Axis.horizontal,
-              onPageChanged: (index, _) {
-                setState(() {
-                  currentIndex = index;
-                });
-              },
-            ),
-            items: imageList.map((imageUrl) {
-              return Container(
-                width: double.infinity,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    imageUrl,
-                    fit: BoxFit.cover,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            CarouselSlider(
+              carouselController: _carouselController,
+              options: CarouselOptions(
+                height: double.infinity,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                autoPlay: false,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+                onPageChanged: (index, _) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+              items: imageList.map((imageUrl) {
+                return Container(
+                  width: double.infinity,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-              );
-            }).toList(),
-          ),
-          Positioned(
-            top: 16.0,
-            right: 16.0,
-            child: ElevatedButton(
-              onPressed: () {
-                // Ação do botão "Skip"
-              },
-              child: Text('Skip'),
+                );
+              }).toList(),
             ),
-          ),
-          Positioned(
-            bottom: 16.0,
-            left: 16.0,
-            child: SmoothPageIndicator(
-              count: imageList.length,
-              controller: _pageController, // Substituído _carouselController por _pageController
-              effect: ExpandingDotsEffect(
-                dotColor: Colors.grey,
-                activeDotColor: Colors.black,
-                dotHeight: 8.0,
-                dotWidth: 8.0,
-                spacing: 8.0,
+            Positioned(
+              top: 16.0,
+              right: 16.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Ação do botão "Skip"
+                },
+                child: Text('Skip'),
               ),
             ),
-          ),
-          Positioned(
-            right: 16.0,
-            bottom: 16.0,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  currentIndex = (currentIndex + 1) % imageList.length;
-                  _carouselController.nextPage();
-                });
-              },
-              child: Text('Next'),
+            Positioned(
+              bottom: 16.0,
+              left: 16.0,
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: imageList.length,
+                effect: ExpandingDotsEffect(
+                  dotColor: Colors.grey,
+                  activeDotColor: Colors.black,
+                  dotHeight: 8.0,
+                  dotWidth: 8.0,
+                  spacing: 8.0,
+                ),
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              right: 16.0,
+              bottom: 16.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    currentIndex = (currentIndex + 1) % imageList.length;
+                    _carouselController.nextPage();
+                  });
+                },
+                child: Text('Next'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
