d4517518de2d018f165ca7a20b067141eb5cc184
RobsonSousaS <robson.silva09@aluno.ifce.edu.br>
2023-08-11 14:25:16 -0300

    NOVAS ADIÇÕES!

--- lib/Imports/imports.dart
+++ lib/Imports/imports.dart
@@ -1,20 +1,23 @@
 export 'package:flutter/material.dart';
 export 'package:bank_darm/widgets/widgetspaget.dart';
-export 'package:bank_darm/pages/slider.dart';
-export 'package:bank_darm/pages/login.dart';
+export 'package:bank_darm/pages/client/slider.dart';
+export 'package:bank_darm/pages/client/login.dart';
 export 'package:smooth_page_indicator/smooth_page_indicator.dart';
-export 'package:bank_darm/pages/login_signup.dart';
+export 'package:bank_darm/pages/client/login_signup.dart';
 export 'package:google_fonts/google_fonts.dart';
-export 'package:bank_darm/pages/signup.dart';
-export 'package:bank_darm/pages/forgotpass.dart';
-export 'package:bank_darm/pages/listcards.dart';
-export 'package:bank_darm/pages/usercli.dart';
+export 'package:bank_darm/pages/client/signup.dart';
+export 'package:bank_darm/pages/client/forgotpass.dart';
+export 'package:bank_darm/pages/client/listcards.dart';
+export 'package:bank_darm/pages/client/usercli.dart';
 export 'package:brasil_fields/brasil_fields.dart';
 export 'package:flutter/services.dart';
 export 'package:firebase_core/firebase_core.dart';
-export 'package:bank_darm/firebase_options.dart';
-export 'package:bank_darm/pages/carddemonst.dart';
-export 'package:bank_darm/administrator/listclients.dart';
-export 'package:bank_darm/administrator/listcardsclient.dart';
+export 'package:bank_darm/firebase/firebase_options.dart';
+export 'package:bank_darm/pages/client/carddemonst.dart';
+export 'package:bank_darm/pages/administrator/listclients.dart';
+export 'package:bank_darm/pages/administrator/listcardsclient.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:firebase_auth/firebase_auth.dart';
+import 'package:go_router/go_router.dart';
+
+
